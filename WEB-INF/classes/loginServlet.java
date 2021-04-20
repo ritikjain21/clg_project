9931bebc5230787fb13c0612da75a2543ad1e4c1
Ritik <ritikjain123321@gmail.com>
2021-04-20 12:18:09 +0530

    first commit

---
+++ WEB-INF/classes/loginServlet.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class loginServlet extends HttpServlet
+{
+	public void doPost(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+	{
+		String s1 = request.getParameter("u1");
+		String s2 = request.getParameter("u2");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clg_project","root","root");
+			Statement st = con.createStatement();
+
+            String q = "select * from registereduser where (NAME='"+s1+"' AND PASSWORD='"+s2+"') ";
+			
+			ResultSet rs = st.executeQuery(q);
+		
+			if(rs.next())
+			{
+				 response.sendRedirect("Home2.html");			
+			}
+		    else{
+			 response.sendRedirect("Login.html");			
+		    }
+			
+			con.close();
+		}
+		catch(Exception e){
+			out.println(e);
+		}
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
---
+++ WEB-INF/classes/regServlet.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class regServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest request,HttpServletResponse response)throws IOException,ServletException
+	{
+		String s1 = request.getParameter("u1");
+		String s2 = request.getParameter("u2");
+		String s3 = request.getParameter("u3");
+		String s4 = request.getParameter("u4");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/clg_project","root","root");
+			Statement st = con.createStatement();
+			
+			String q ="insert into registereduser values ('"+s1+"','"+s2+"','"+s3+"','"+s4+"') ";
+		    st.executeUpdate(q);
+            
+            response.sendRedirect("Login.html");			
+			
+			con.close();
+		}
+		catch(Exception e)
+		{
+			out.println(e);
+		}
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
